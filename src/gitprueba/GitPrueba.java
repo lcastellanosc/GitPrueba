49c14bb8d7e70c3da9f51265709ea5d377d3b29b
lorena <>
2023-11-30 10:48:33 +0100

    cambiado el principal

---
+++ src/gitprueba/GitPrueba.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gitprueba;
+
+/**
+ *
+ * @author ProfDiurno
+ */
+public class GitPrueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Estoy haciendo un cambio");
+        //https://github.com/lcastellanosc/GitPrueba.git
+    }
+    
+}
